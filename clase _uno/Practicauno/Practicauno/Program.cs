8b16c0612c25a91d2ecba97cf3f8c3392bd87177
Yael Castaneda <60015738+yaelcast@users.noreply.github.com>
2020-01-27 12:30:18 -0700

    Practica uno
    Practica con condicionales if and else

--- clase _uno/Practicauno/Practicauno/Program.cs
+++ clase _uno/Practicauno/Practicauno/Program.cs
@@ -4,9 +4,24 @@ namespace Practicauno
 {
     class Program
     {
+        //Variable de la clase
+        static string nombre = "Yael";
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            //Respondemos con el primer argumento y el segundo.
+            //Condicion se debe de cumplir todas las sentencias si utilizan &&.
+            //Condicion se debe de cumplir cualquier de las sentencias si utilizan ll.
+            //Tipos de comparacion > < == >= <= !=
+
+            if (args[0].Length > 0 && args[0] == nombre) //true o false.
+            {
+
+                Console.WriteLine($"Hello {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine("User unathorized");
+            }
         }
     }
 }
