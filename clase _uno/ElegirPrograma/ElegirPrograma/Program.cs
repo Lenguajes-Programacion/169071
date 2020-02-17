cd0b72cc859ceea5f0bdf9c60565fdeb4ce553ce
Yael Castaneda <60015738+yaelcast@users.noreply.github.com>
2020-02-17 03:06:51 -0700

    Add files via upload

---
+++ clase _uno/ElegirPrograma/ElegirPrograma/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElegirPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eleccion = "";
+            string continuar = "si"; 
+            //Le doy valor desde un inicio para que haga la entrada
+
+            while(continuar=="si")
+            {
+                Console.WriteLine("\nPROGRAMA PRINCIPAL");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Elija el programa que desea usar");
+                Console.WriteLine("1 para Calculadora");
+                Console.WriteLine("2 para Nombre");
+
+                eleccion = Console.ReadLine();
+
+                switch(eleccion)
+                {
+                    case "1":
+                        Calculadora c1 = new Calculadora();
+                        c1.Usar();
+                        break;
+
+                    case "2":
+                        Name c2 = new Name();
+                        c2.Yael();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no disponible");
+                        break;
+                }
+
+                Console.WriteLine("\nUsted esta en el programa principal");
+                Console.WriteLine("¿Desea continuar?");
+                continuar = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
